fff09d9d574dfba5ff509983e15f24a4f47236fd
Rajveer34 <153397604+Rajveer34@users.noreply.github.com>
2023-12-17 07:26:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['14159443385'] //ur owner number
+global.ownername = "╰┈➤ ❝ [𝐏𝐫𝐚𝐝𝐞𝐞𝐩] ❞" //ur owner name
+global.ytname = "╰┈➤ ❝ [𝐏𝐫𝐚𝐝𝐞𝐞𝐩] ❞" //ur yt chanel name
+global.socialm = "GitHub: DGXeon" //itspradeep_kmt
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['14159443385']
+global.ownertag = '14159443385'
+global.botname = '╰┈➤ ❝ [𝐏𝐫𝐚𝐝𝐞𝐞𝐩] ❞'
+global.linkz = "╰┈➤ ❝ [𝐏𝐫𝐚𝐝𝐞𝐞𝐩] ❞" //your theme url which will be displayed on whatsapp
+global.websitex = "╰┈➤ ❝ [𝐏𝐫𝐚𝐝𝐞𝐞𝐩] ❞" //ur website to be displayed
+global.botscript = 'https://github.com/rajveer34/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "╰┈➤ ❝ [𝐏𝐫𝐚𝐝𝐞𝐞𝐩] ❞" //ur sticker watermark packname
+global.author = "╰┈➤ ❝ [𝐏𝐫𝐚𝐝𝐞𝐞𝐩] ❞" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
